48b3391b33fd8636258c2c3fd10923e1076b8255
nuhuxi <mark.keckeis@gmail.com>
2016-03-28 16:53:46 -0700

    Before Authentication

--- public/app/app.js
+++ public/app/app.js
@@ -4,9 +4,6 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 							$locationProvider.html5Mode(true);
 							$routeProvider
 								.when('/', {templateUrl: '/partials/main', 
-											controller: 'mainCtrl'});
+											controller: 'mvMainCtrl'});
 								});
 
-angular.module('app').controller('mainCtrl', function($scope){
-	$scope.myVar = "Hello Angular";
-});
