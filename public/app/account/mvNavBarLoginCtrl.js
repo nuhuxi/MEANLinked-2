f535e70f57350454e97a78408f9a3ad0055b77a6
nuhuxi <mark.keckeis@gmail.com>
2016-03-31 18:34:27 -0700

    Before Imroving the Server Side COde - all working

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,13 +1,13 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
 	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
-		$http.post('/login', {username: username, password: password}).then(function(response){
-			if(response.data.success){
-				mvIdentity.currentUser = response.data.user;
-				mvNotifier.notify('You have logged in.');
+		mvAuth.authenticateUser(username, password).then(function(success) {
+			if(success) {
+				mvNotifier.notify('You have successfully logged in.');
 			} else {
 				mvNotifier.notify('Username/Password are incorrect.');
 			}
+
 		});
 	};
 
