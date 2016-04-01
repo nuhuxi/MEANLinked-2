da6f64135876d4db16adc979e2fff996b4d9c27d
nuhuxi <mark.keckeis@gmail.com>
2016-04-01 11:45:10 -0700

    Before Persisting Login between Page Refreshes

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
 	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
 		mvAuth.authenticateUser(username, password).then(function(success) {
@@ -8,6 +8,14 @@ angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mv
 				mvNotifier.notify('Username/Password are incorrect.');
 			}
 
+		});
+	};
+	$scope.signout = function(){
+		mvAuth.logoutUser().then(function(){
+			$scope.username = "";
+			$scope.password = "";
+			mvNotifier.notify("You have successfully logged out.");
+			$location.path('/');
 		});
 	};
 
