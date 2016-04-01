419b4ec5fc32d9446b920a61a191374026f04a8f
nuhuxi <mark.keckeis@gmail.com>
2016-04-01 11:26:14 -0700

    Before adding signout functionality

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'), 
+	crypto = require('crypto');
 
 module.exports = function(config){
 	mongoose.connect(config.db);
@@ -11,17 +12,67 @@ module.exports = function(config){
 	var userSchema = mongoose.Schema({
 		firstName: String, 
 		lastName: String, 
-		userName: String
+		userName: String, 
+		salt: String, 
+		hashed_pwd: String
 	});
+
+	userSchema.methods = {
+		authenticate: function(passwordToMatch){
+			return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+		}
+	};
 	var User = mongoose.model('User', userSchema);
 
 	User.find({}).exec(function(err, collection){
 		if(collection.length === 0){
-			User.create({firstName:'Mark', lastName: 'Keckeis', userName: 'mark'});
-			User.create({firstName:'Mardi', lastName: 'Keckeis', userName: 'mardi'});
-			User.create({firstName:'Luke', lastName: 'Keckeis', userName: 'luke'});
-			User.create({firstName:'Max', lastName: 'Keckeis', userName: 'max'});
-			User.create({firstName:'Grace', lastName: 'Keckeis', userName: 'grace'});
+			var salt, hash;
+
+			salt = createSalt();
+			hash = hashPwd(salt, 'mark');
+			User.create({firstName:'Mark', lastName: 'Keckeis', userName: 'mark', salt: salt, hashed_pwd: hash});
+
+			salt = createSalt();
+			hash = hashPwd(salt, 'mardi');
+			User.create({firstName:'Mardi', lastName: 'Keckeis', userName: 'mardi', salt: salt, hashed_pwd: hash});
+			salt = createSalt();
+			hash = hashPwd(salt, 'luke');
+			User.create({firstName:'Luke', lastName: 'Keckeis', userName: 'luke', salt: salt, hashed_pwd: hash});
+
+			salt = createSalt();
+			hash = hashPwd(salt, 'max');
+			User.create({firstName:'Max', lastName: 'Keckeis', userName: 'max', salt: salt, hashed_pwd: hash});
+
+			salt = createSalt();
+			hash = hashPwd(salt, 'grace');
+			User.create({firstName:'Grace', lastName: 'Keckeis', userName: 'grace', salt: salt, hashed_pwd: hash});
 		}
 	});
 };
+
+function createSalt(){
+	return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+  hmac.setEncoding('hex');
+  hmac.write(pwd);
+    console.log("hmac is " + hmac);
+  hmac.end();
+  return hmac.read();
+}
+/*
+// This is the code he says is deprecated
+function hashPwd(salt, pwd) {
+	var hmac = crypto.createHmac('sha1', salt);
+	return hmac.update(pwd).digest('hex');
+
+}
+*/
+
+
+
+
+
+
