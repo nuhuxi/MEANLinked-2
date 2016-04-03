f07b86add4423ef7b4298958fca10d6ab82c786f
nuhuxi <mark.keckeis@gmail.com>
2016-04-03 16:54:06 -0700

    At Protecting Admin Pages on the Client at 634

--- public/app/app.js
+++ public/app/app.js
@@ -4,5 +4,24 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 							$locationProvider.html5Mode(true);
 							$routeProvider
 								.when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
-								.when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'mvUserListCtrl'})
-								});
+								.when('/admin/users', {templateUrl: '/partials/admin/user-list',
+									controller: 'mvUserListCtrl',
+									resolve: {
+										auth: function(mvIdentity, $q){
+											if(mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1){
+												return true;
+											} else {
+												return $q.reject('not authorized');
+											}
+										}
+									}
+								})
+});
+
+angular.module('app').run(function($rootScope, $location) {
+	$rootScope.$on('$routeChangeError', function(evt, current, previous, rejection){
+		if(rejection === 'not authorized'){
+			$location.path('/');
+		}
+	})
+})
