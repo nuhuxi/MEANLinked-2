fa55f9b65ad762ec6411be57702849189dcc42b8
nuhuxi <mark.keckeis@gmail.com>
2016-04-03 16:42:00 -0700

    ProtectingADmin Pages on the Client - 257

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,6 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
 							$locationProvider.html5Mode(true);
 							$routeProvider
-								.when('/', {templateUrl: '/partials/main/main', 
-											controller: 'mvMainCtrl'});
+								.when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+								.when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'mvUserListCtrl'})
 								});
-
