9e334e928bdb8f1e99a00587e9d46d502305c0e1
nuhuxi <mark.keckeis@gmail.com>
2016-04-05 13:52:53 -0700

    At the end of Implementing Model validation

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-	encrypt = require('../utilities/encryption');
+	userModel = require('../models/user');
 
 module.exports = function(config){
 	mongoose.connect(config.db);
@@ -9,44 +9,5 @@ module.exports = function(config){
 		console.log('multivision db opened');
 	});
 
-	var userSchema = mongoose.Schema({
-		firstName: String,
-		lastName: String,
-		username: String,
-		salt: String,
-		hashed_pwd: String,
-		roles: [String]
-	});
-
-	userSchema.methods = {
-		authenticate: function(passwordToMatch){
-			return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-		}
-	};
-	var User = mongoose.model('User', userSchema);
-
-	User.find({}).exec(function(err, collection){
-		if(collection.length === 0){
-			var salt, hash;
-
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'mark');
-			User.create({firstName:'Mark', lastName: 'Keckeis', username: 'mark', salt: salt, hashed_pwd: hash, roles: ['admin']});
-
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'mardi');
-			User.create({firstName:'Mardi', lastName: 'Keckeis', username: 'mardi', salt: salt, hashed_pwd: hash, roles: []});
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'luke');
-			User.create({firstName:'Luke', lastName: 'Keckeis', username: 'luke', salt: salt, hashed_pwd: hash});
-
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'max');
-			User.create({firstName:'Max', lastName: 'Keckeis', username: 'max', salt: salt, hashed_pwd: hash});
-
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'grace');
-			User.create({firstName:'Grace', lastName: 'Keckeis', username: 'grace', salt: salt, hashed_pwd: hash});
-		}
-	});
+	userModel.createDefaultusers();
 };
